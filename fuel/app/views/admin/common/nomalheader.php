32312472ea0b02cfa1e49e37d39d1c7e1162bf11
esmile-sys <e.smile.sys.php@gmail.com>
2017-06-26 17:16:26 +0900

    ヘッダフッタファイル名変更

--- fuel/app/views/admin/common/nomalheader.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-	<title>e-smile-sys</title>
-	<?php echo Asset::css("style.css"); ?>
-</head>
-<body>
-	<div id="main">
